68d0c0e8336848dd194cbfdbe8bfeeb812a5118e
vishalxh <140748714+vishalxh@users.noreply.github.com>
2024-10-17 01:23:33 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 import project1 from "../../public/assets/projects/chatapp.jpg";
 import project2 from "../../public/assets/projects/Movieflix2.jpg";
 import project3 from "../../public/assets/projects/Portfolio.png";
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. After making some projects I have honed my skills in front-end technologies like Reactjs, as well as back-end technologies like Node.js, Express, Mongodb, and Socket.io. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications.I have worked with a variety of technologies, including React, Node.js, express and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I enjoy solving complex problems to deliver high-quality solutions.`;
 
